17ab12d0bed0b895594e37603b6f0a92330bb316
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-01-24 21:58:40 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,189 @@
+/**
+ * Define Global Variables
+ *
+ */
+const navList = document.getElementById("navbar__list");
+const sections = document.getElementsByTagName("section");
+const fregNavList = document.createDocumentFragment();
+const scrollBtn = document.getElementById("scroll-btn");
+const hamburgerIcon = document.getElementById("hamburger-icon");
+
+// variable to store the active section throughout the code
+let activeSection = document.querySelector(".your-active-class");
+// variable to store the active anchor tag
+let activeAnchor;
+let navElementCount = 0;
+let lastScrollTop = 0;
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+/**
+ * @description Create nav element
+ * @param {string} sectionName
+ * @param {string} sectionId
+ * @returns {Node} list item
+ */
+function createNavElement(secName) {
+  const navElement = document.createElement("li");
+  const anchorTag = document.createElement("a");
+  navElementCount++;
+
+  anchorTag.setAttribute("id", "anchor" + navElementCount);
+  anchorTag.classList.add("menu__link");
+  anchorTag.textContent = secName;
+  anchorTag.style.cursor = "pointer";
+
+  navElement.appendChild(anchorTag);
+
+  return navElement;
+}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+/**
+ * @description Build the nav
+ * @param {HTMLCollection} sections
+ */
+function buildNavBar(sections) {
+  for (let section of sections) {
+    const secName = section.getAttribute("data-nav");
+    const secId = section.getAttribute("id");
+
+    fregNavList.appendChild(createNavElement(secName, secId));
+  }
+  navList.appendChild(fregNavList);
+
+  // set the first link as active link
+  const activeAnch = document.getElementById("anchor1");
+  activeAnch.classList.add("menu__link--active");
+  activeAnchor = activeAnch;
+}
+
+/**
+ * @description  Set section and its anchor tag as active
+ * @param {Node} inactiveSection
+ */
+function activateSection(inactiveSec) {
+  if (inactiveSec !== activeSection) {
+    // activate section
+    inactiveSec.classList.add("your-active-class");
+    activeSection.classList.remove("your-active-class");
+    activeSection = inactiveSec;
+
+    // activate anchor tag
+    let anchorId = inactiveSec.getAttribute("id");
+    anchorId = anchorId.replace("section", "anchor");
+    const anchor = document.getElementById(anchorId);
+
+    anchor.classList.add("menu__link--active");
+    activeAnchor.classList.remove("menu__link--active");
+    activeAnchor = anchor;
+  }
+}
+
+// Add class 'active' to section when near top of viewport
+onscroll = function () {
+  let scrollPoss = this.document.documentElement.scrollTop;
+
+  for (let section of sections) {
+    if (
+      scrollPoss >= section.offsetTop - 100 &&
+      scrollPoss <= section.offsetTop - 200 + section.offsetHeight
+    ) {
+      activateSection(section);
+    }
+  }
+};
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
+buildNavBar(sections);
+
+// Scroll to section on link click
+navList.addEventListener("click", (e) => {
+  if (e.target.nodeName === "A") {
+    const sec = document.querySelector(
+      `[data-nav = '${e.target.textContent}']`
+    );
+    const secPoss = sec.getBoundingClientRect().top;
+    const startPoss = window.pageYOffset;
+    activateSection(sec);
+
+    window.scrollTo({
+      top: secPoss + startPoss,
+      behavior: "smooth",
+    });
+
+    navList.classList.toggle("visible");
+  }
+});
+
+// hide the nav while scroll down and show it while scroll up
+window.addEventListener("scroll", function () {
+  let st = window.pageYOffset || document.documentElement.scrollTop;
+  const nav = document.getElementById("navbar__list");
+
+  if (st > lastScrollTop) {
+    nav.style.top = "-150px";
+  } else {
+    nav.style.top = "0";
+  }
+
+  lastScrollTop = st <= 0 ? 0 : st;
+});
+
+/* Change the nav style when scroll */
+const nav = document.getElementById("navbar__list");
+const sectionOne = document.getElementById("section1");
+
+const sectionOneOptions = {
+  rootMargin: "-400px 0px 0px 0px",
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      nav.classList.add("nav-scrolled");
+    } else {
+      nav.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+
+/* form focus state */
+
+var inputs = document.getElementsByTagName('input');
+
+for (var i = 0; i < inputs.length; i++) {
+  var input = inputs[i];
+  input.addEventListener('input', function() {
+    this.value ? this.classList.add('notempty'): this.classList.remove('notempty');
+
+  });
+}
+
+
+var textarea = document.getElementsByTagName('textarea')[0];
+
+textarea.addEventListener('input', function() {
+  this.value ? this.classList.add('notempty'): this.classList.remove('notempty');
+});
